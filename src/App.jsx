60c94bffd1f208f1972287222c0b1f2f6b575fd7
aymen <aymenbendhaiba4@gmail.com>
2024-11-07 16:06:53 +0100

    events club listing & details

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import '../index.css';
+import "../index.css";
 import DemandesListing from "./features/demande listing/demande.listing.jsx";
 import DemandeDetails from "./features/demande details/demande.details.jsx";
+import ClubsListingPage from "./features/clubs listing/clubs.listing.page.jsx";
+import EventsListing from "./features/events listing/event.listing.jsx";
+import ClubDetails from "./features/club details/club.details.jsx";
+import EventDetails from "./features/event details/event.details.jsx";
 
 function App() {
-
-
   return (
     <>
-        <DemandeDetails></DemandeDetails>
+      <EventDetails></EventDetails>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
