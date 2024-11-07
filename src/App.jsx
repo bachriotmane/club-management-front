fe8cfcf5f63bf68638cffc79b129ea5fa28e0e75
Aramali Mohammed <mouhammedaramali@gmail.com>
2024-11-07 17:39:45 +0100

    add the auth components

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,29 @@ import ClubsListingPage from "./features/clubs listing/clubs.listing.page.jsx";
 import EventsListing from "./features/events listing/event.listing.jsx";
 import ClubDetails from "./features/club details/club.details.jsx";
 import EventDetails from "./features/event details/event.details.jsx";
-
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+import PrivateRoute from "./shared/components/auth/PrivateRoute.jsx";
+import Login from "./shared/components/auth/Login.jsx";
+import Unauthorized from "./shared/components/auth/Unauthorized.jsx";
+import NotFound from "./shared/components/auth/NotFound.jsx";
 function App() {
   return (
-    <>
-      <EventDetails></EventDetails>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/" element={<DemandesListing />} />
+        <Route path="/demande/:id" element={<DemandeDetails />} />
+        <Route path="/club/:id" element={<ClubDetails />} />
+        <Route path="/events" element={<EventsListing />} />
+        <Route path="/event/:id" element={<EventDetails />} />
+
+        <Route element={<PrivateRoute roles={['ROLE_ADMIN']} />}>
+          <Route path="/clubs" element={<ClubsListingPage />} />
+        </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
   );
 }
 
