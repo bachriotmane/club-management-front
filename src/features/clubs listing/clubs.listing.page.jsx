743b96413aab0cbfb2e17e8bb02a75ae2494dcce
bachriotmane <bachriotmane@gmail.com>
2024-11-10 20:08:05 +0100

    add item

--- src/features/clubs listing/clubs.listing.page.jsx
+++ src/features/clubs listing/clubs.listing.page.jsx
@@ -32,7 +32,7 @@ const ClubsListingPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {clubs.map((club) => (
-          <ClubCard key={club.id} club={club} />
+          <ClubCard key={club.id} item={club} />
         ))}
       </div>
     </div>
--- src/features/events listing/event.listing.jsx
+++ src/features/events listing/event.listing.jsx
@@ -44,7 +44,7 @@ const EventsListing = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {events.map((event) => (
-          <EventCard key={event.id} event={event} />
+          <EventCard key={event.id} item={event} />
         ))}
       </div>
     </div>
