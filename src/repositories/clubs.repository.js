1c27ce639e1e49d52eab3dc485aa8b95b2e2a54a
soufiane-bourich <soufianbourich20@gmail.com>
2024-11-17 11:18:58 +0100

    finishing branchement api details club by uuid

--- src/repositories/clubs.repository.js
+++ src/repositories/clubs.repository.js
@@ -2,7 +2,7 @@ import axiosInstance from "../auth/axios.js";
 
 const apiUrl = "/clubs";
 
-export const getClubs = async ({ page, size = 3, nomClub = "", idUser = 0 }) => {
+export const getClubs = async ({ page, size = 3, nomClub = "", idUser = "" }) => {
   try {
     const response = await axiosInstance.get(apiUrl, {
       params: {
@@ -12,6 +12,30 @@ export const getClubs = async ({ page, size = 3, nomClub = "", idUser = 0 }) => 
         idUser: idUser,
       },
     });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const getClubById = async (uuid) => {
+  try {
+    const response = await axiosInstance.get(`${apiUrl}/club/${uuid}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getClubMembers = async ({ uuidClub, page, size, studentName }) => {
+  try {
+    const response = await axiosInstance.get(
+      `${apiUrl}/${uuidClub}/members`,
+      {
+        params: { page, size, studentName },
+      }
+    );
     return response.data;
   } catch (error) {
     throw error;
