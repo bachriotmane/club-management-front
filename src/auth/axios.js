2c1e0e85253d0620a2359ccd8fcc3b3f763bed99
ouyazoussama <ouyazoussama@gmail.com>
2024-11-22 22:32:50 +0100

    :)

--- src/auth/axios.js
+++ src/auth/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { logout } from './auth';
 
-const API_URL = import.meta.env.VITE_API_URL|| 'http://localhost:8088';
+const API_URL = import.meta.env.VITE_API_URL|| 'http://localhost:8080';
 const axiosInstance = axios.create({
     baseURL: API_URL,
     headers: {
