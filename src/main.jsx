e78d518ac0a69872516417334818b2710c99ce53
Idrissi <idrissiabdelkarim01@gmail.com>
2024-11-22 17:16:03 +0100

    disabling ReactQueryDevtools button

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')).render(
       <UserProvider>
         <App />
       </UserProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </React.StrictMode>
 );
