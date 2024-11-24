b3f593faf401cfc63f1038d5a3662dda68513bb2
soufiane-bourich <soufianbourich20@gmail.com>
2024-11-24 15:34:33 +0100

    Fix : probleme Listing membres

--- src/features/club members listing/ClubMembersListing.jsx
+++ src/features/club members listing/ClubMembersListing.jsx
@@ -420,7 +420,7 @@ const ClubMembersListing = () => {
 
 {errorRed && (
   <div className="fixed bottom-4 right-4 bg-red-500 text-white px-4 py-2 rounded-lg flex items-center">
-    <span>{errorDelete}</span>
+    <span>{errorRed}</span>
     <button className="ml-2" onClick={() => closeMessage(setErrorRed)}>
       <IoMdClose size={20} />
     </button>
