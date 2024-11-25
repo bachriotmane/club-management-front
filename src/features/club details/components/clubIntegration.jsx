5f1cdbafcca8cc18a441830b1789c7acdd1b9bbb
bachriotmane <bachriotmane@gmail.com>
2024-11-26 00:04:44 +0100

    handle navigate back

--- src/features/club details/components/clubIntegration.jsx
+++ src/features/club details/components/clubIntegration.jsx
@@ -8,7 +8,7 @@ const ClubIntegration = ({ club, navigate }) => {
         <button
           onClick={() =>
             navigate("/demandes/deposer", {
-              state: { clubId: club.uuid, clubName: club.nom }
+              state: { clubId: club.uuid, clubName: club.nom, regenerate: true}
             })
           }
           className="flex items-center px-4 py-2 text-blue-600 rounded-lg border-2 border-blue-600 hover:bg-blue-600 hover:text-white hover:border-blue-700 transition-all duration-300"
